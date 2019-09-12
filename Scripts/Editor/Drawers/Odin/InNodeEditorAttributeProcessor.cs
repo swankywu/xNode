2462ecc7acb056b58b4ba25146a7d0783243181d
swanky <swanky.wu@gmail.com>
2019-09-12 18:13:32 +0800

    add processor for the graph objects;

--- Scripts/Editor/Drawers/Odin/InNodeEditorAttributeProcessor.cs
+++ Scripts/Editor/Drawers/Odin/InNodeEditorAttributeProcessor.cs
@@ -44,5 +44,39 @@ namespace XNodeEditor {
 			}
 		}
 	}
+
+	internal class OdinNodeGraphInGraphAttributeProcessor<T> : OdinAttributeProcessor<T> where T : NodeGraph {
+		public override bool CanProcessSelfAttributes(InspectorProperty property) {
+			return false;
+		}
+
+		public override bool CanProcessChildMemberAttributes(InspectorProperty parentProperty, MemberInfo member) {
+			if (!NodeEditor.inNodeEditor && !NodeEditor.inOthers)
+				return false;
+
+			if (member.MemberType == MemberTypes.Field) {
+				switch (member.Name) {
+					case "nodes":
+						return true;
+
+					default:
+						break;
+				}
+			}
+
+			return false;
+		}
+
+		public override void ProcessChildMemberAttributes(InspectorProperty parentProperty, MemberInfo member, List<Attribute> attributes) {
+			switch (member.Name) {
+				case "nodes":
+					attributes.Add(new HideInInspector());
+					break;
+
+				default:
+					break;
+			}
+		}
+	}
 }
 #endif
